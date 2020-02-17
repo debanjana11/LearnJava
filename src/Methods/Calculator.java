0beb0235f7f9b3177e637d4c95627e156a3ded3a
Debanjana Gupta <debanjanagupta.94@gmail.com>
2020-02-17 23:05:09 +0530

    Added class for calculator

---
+++ src/Methods/Calculator.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class Calculator {
+	public static void main(String[] args) {
+    int sum=add(45, 89);
+    System.out.println(sum);
+	}
+	public static int add(int x, int y) {
+		int z=x+y;
+		return z;
+	}
+    
+}
