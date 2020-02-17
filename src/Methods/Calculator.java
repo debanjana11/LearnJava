089757f3f43fac7db74ea7d7df84a51f158a259b
Debanjana Gupta <debanjanagupta.94@gmail.com>
2020-02-17 23:18:30 +0530

    indentation

--- src/Methods/Calculator.java
+++ src/Methods/Calculator.java
@@ -2,8 +2,8 @@ package Methods;
 
 public class Calculator {
 	public static void main(String[] args) {
-    int sum = add(45, 89);
-    System.out.println(sum);
+		int sum = add(45, 89);
+		System.out.println(sum);
 	}
 	public static int add(int x, int y) {
 		int z = x + y;
